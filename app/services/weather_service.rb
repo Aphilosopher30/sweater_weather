5e2e102d4cfe7a24bd91cfd607453cb07254586c
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-06-13 15:55:36 -0600

    addjust weather api call to exclude some unessesary data

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -10,11 +10,12 @@ class WeatherService
     JSON.parse(resp.body, symbolize_names: true)
   end
 
-
   def self.get_weather(coordinates)
     response = connection.get('/data/2.5/onecall') do |faraday|
       faraday.params[:lat] = coordinates[:lat]
       faraday.params[:lon] = coordinates[:lng]
+      faraday.params[:exclude] = 'minutely,alerts'
+
     end
 
     parse_json(response)
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe WeatherService do
 
       json_response = File.read('spec/fixtures/weather/weather_report.json')
 
-      stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=39.738453&lon=-104.984853&appid=#{ENV['weather_key']}").
+      stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=39.738453&lon=-104.984853&appid=#{ENV['weather_key']}&exclude=minutely,alerts").
         with(
           headers: {
          'Accept'=>'*/*',
