956441facaf88b581f529f45b872cd01bf474263
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-06-13 15:37:00 -0600

    include test

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe WeatherService do
   describe "returns weather data" do
     it "returns the desired data" do
 
-# binding.pry
       json_response = File.read('spec/fixtures/weather/weather_report.json')
-
 
       stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?lat=39.738453&lon=-104.984853&appid=#{ENV['weather_key']}").
         with(
