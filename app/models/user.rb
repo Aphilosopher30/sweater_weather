ac9237fac9a2114b523fdca5c567685949dd6bdb
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-06-16 00:05:09 -0600

    set up and test validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  # validates :email, presence: true, uniqueness: true
-  # validates :password_digest, presence: true
-  # # validates :password, confirmation: true, presence: true, on: :create
-  
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, confirmation: true, presence: true, on: :create
+
   has_secure_password
 end
